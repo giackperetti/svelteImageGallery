988ccf47d54ccea28f3817bccbb61d0f4b30a3b9
giackperetti <github.giack@gmail.com>
2024-07-22 16:45:36 +0200

    removed hardcoded paths

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,14 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
-export const images = {
-  image_1: '/gallery/Fern.png',
-  image_2: '/gallery/Flamme.png',
-  image_3: '/gallery/frieren-blue moon flowers-upscaled.jpg',
-  image_4: '/gallery/frieren-blue moon flowers.jpg',
-  image_5: '/gallery/frieren-falling comets-upscaled.jpg',
-  image_6: '/gallery/frieren-falling comets.jpg',
-  image_7: '/gallery/frieren-fern_meal-sunset.png',
-  image_8: '/gallery/frieren-lying in river.jpg',
-  image_9: '/gallery/frieren-sunrise.jpg',
-  image_10: '/gallery/Frieren.png',
-  image_11: '/gallery/Ubel.png'
+export async function fetchImages() {
+  const res = await fetch('/api/gallery');
+  const data = await res.json();
+  return data.images;
 }
