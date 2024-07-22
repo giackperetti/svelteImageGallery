6addfb4119ff7d33ad2dff7d47fd8dbf3942e1f4
giackperetti <github.giack@gmail.com>
2024-07-22 16:45:57 +0200

    make use of the new API endpoint to fetch the images

--- src/routes/Gallery.svelte
+++ src/routes/Gallery.svelte
@@ -1,45 +1,51 @@
 <script>
-  import { images } from "../lib/index.ts";
+    import { onMount } from 'svelte';
+    import { fetchImages } from '$lib/index.ts';
 
- const numberOfImages = Object.keys(images).length; 
+    let images = [];
+    let numberOfImages = 0;
+
+    onMount(async () => {
+        images = await fetchImages();
+        numberOfImages = images.length;
+    });
 </script>
 
 <p>Number of images: {numberOfImages}</p>
 <div id="gallery-container">
-    {#each Object.entries(images) as [id, image]}
-      <img class="image" src={image} alt={id}>
+    {#each images as { id, src }}
+        <img class="image" src={src} alt={id}>
     {/each}
 </div>
 
 <style>
-  #gallery-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    gap: 10px;
-    padding: 10px;
-  }
-
-  .image {
-    flex: 1 1 45%; 
-    margin: 5px;
-    border: 2px solid #ebdbb2;
-    border-radius: 5px;
-    overflow: hidden;
-    width: 100%;
-    height: auto;
-  }
-
-  @media (min-width: 600px) {
-    .image {
-      flex-basis: calc(50% - 10px);
+    #gallery-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 10px;
+        padding: 10px;
     }
-  }
 
-  @media (min-width: 900px) {
     .image {
-      flex-basis: calc(25% - 10px);
+        flex: 1 1 45%;
+        margin: 5px;
+        border: 2px solid #ebdbb2;
+        border-radius: 5px;
+        overflow: hidden;
+        width: 100%;
+        height: auto;
     }
-  }
+
+    @media (min-width: 600px) {
+        .image {
+            flex-basis: calc(50% - 10px);
+        }
+    }
+
+    @media (min-width: 900px) {
+        .image {
+            flex-basis: calc(25% - 10px);
+        }
+    }
 </style>
-
